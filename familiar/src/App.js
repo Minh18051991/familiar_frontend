31a55f6f04a85b9dd42c8fbef2cb49988e322aa2
hungnguyen988 <hungnguuen988@gmail.com>
2025-01-03 10:09:04 +0700

    hung up jwt

--- familiar/src/App.js
+++ familiar/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HeaderComponent from "./component/header/HeaderComponent";
+import {Route, Routes} from "react-router-dom";
+import LoginComponent from "./component/login/LoginComponent";
+import HomeComponent from "./component/login/HomeComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import RegisterComponent from "./component/register/RegisterComponent";
+import {ToastContainer} from "react-toastify";
+import DetailComponent from "./component/user/DetailComponent";
+import PrivateRoute from "./component/login/PrivateRoute";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderComponent/>
+        <ToastContainer autoClose={1000}/>
+      <Routes>
+
+        <Route path={'/register'} element={<RegisterComponent/>}/>
+        <Route path={'/account'} element={<LoginComponent/>}/>
+          <Route path={'/'} element={<HomeComponent/>}/>
+
+          <Route path="/" element={<PrivateRoute />}>
+              <Route path={'/user/detail/:id'} element={<DetailComponent/>}/>
+              {/* Thêm các route khác cần bảo vệ ở đây */}
+          </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ familiar/src/service/user/user.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+const API_URL =  'http://localhost:8080';
+
+
+export async function getUserById(userId) {
+    const token = localStorage.getItem('token');
+    console.log("token: ", token)
+
+    if (!token) {
+        console.error("No token found");
+        return { error: "Không tìm thấy token xác thực" };
+    }
+
+    try {
+        console.log("--GET USER BY ID");
+
+        const response = await axios.get(`${API_URL}/api/user/detail/${userId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        const userData = response.data;
+        console.log(userData);
+
+        return userData || null;
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+
+        if (error.response) {
+            // Lỗi từ server
+            if (error.response.status === 401) {
+                return { error: "Không có quyền truy cập. Vui lòng đăng nhập lại." };
+            }
+            return { error: error.response.data.message || "Lỗi từ server" };
+        } else if (error.request) {
+            // Lỗi không nhận được phản hồi
+            return { error: "Không thể kết nối đến server" };
+        } else {
+            // Lỗi khác
+            return { error: "Đã xảy ra lỗi khi lấy thông tin người dùng" };
+        }
+    }
+}
+
+export const updateUser = async (userId, updatedData) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.put(`http://localhost:8080/api/user/update/${userId}`, updatedData,{
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error updating user:", error);
+        throw error;
+    }
+};
+
+export async function createUser(user) {
+
+    try {
+
+        const response = await axios.post('http://localhost:8080/api/user/create',user)
+        const userReponse = response.data;
+        if (userReponse != null) {
+            return userReponse;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        return null;
+    }
+}
