dde6adfe31818ab44ad8d81fd072cb9ce925d5a2
hungnguyen988 <hungnguuen988@gmail.com>
2025-01-05 16:20:18 +0700

    up

--- familiar/src/App.js
+++ familiar/src/App.js
@@ -1,5 +1,43 @@
+// import './App.module.css';
+// import HeaderComponent from "./component/header/HeaderComponent";
+// import {Route, Routes} from "react-router-dom";
+// import LoginComponent from "./component/login/LoginComponent";
+// import HomeComponent from "./component/login/HomeComponent";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import RegisterComponent from "./component/register/RegisterComponent";
+// import {ToastContainer} from "react-toastify";
+// import DetailComponent from "./component/user/DetailComponent";
+// import PrivateRoute from "./component/login/PrivateRoute";
+// import FooterComponent from "./component/footer/FooterComponent";
+//
+//
+// function App() {
+//     return (
+//         <>
+//             <HeaderComponent/>
+//             <ToastContainer autoClose={1000}/>
+//             <Routes>
+//
+//                 <Route path={'/register'} element={<RegisterComponent/>}/>
+//                 <Route path={'/login'} element={<LoginComponent/>}/>
+//                 <Route path={'/'} element={<HomeComponent/>}/>
+//
+//                 <Route path="/" element={<PrivateRoute/>}>
+//                     <Route path={'/user/detail/:id'} element={<DetailComponent/>}/>
+//                     {/* Thêm các route khác cần bảo vệ ở đây */}
+//                 </Route>
+//             </Routes>
+//             <FooterComponent/>
+//         </>
+//     );
+// }
+//
+// export default App;
 
-import './App.css';
+
+import React from 'react';
+import styles from './App.module.css';
 import HeaderComponent from "./component/header/HeaderComponent";
 import {Route, Routes} from "react-router-dom";
 import LoginComponent from "./component/login/LoginComponent";
@@ -10,26 +48,29 @@ import RegisterComponent from "./component/register/RegisterComponent";
 import {ToastContainer} from "react-toastify";
 import DetailComponent from "./component/user/DetailComponent";
 import PrivateRoute from "./component/login/PrivateRoute";
-
+import FooterComponent from "./component/footer/FooterComponent";
+import UpdateAccountComponent from "./component/account/UpdateAccountComponent";
 
 function App() {
-  return (
-    <>
-      <HeaderComponent/>
-        <ToastContainer autoClose={1000}/>
-      <Routes>
-
-        <Route path={'/register'} element={<RegisterComponent/>}/>
-        <Route path={'/account'} element={<LoginComponent/>}/>
-          <Route path={'/'} element={<HomeComponent/>}/>
-
-          <Route path="/" element={<PrivateRoute />}>
-              <Route path={'/user/detail/:id'} element={<DetailComponent/>}/>
-              {/* Thêm các route khác cần bảo vệ ở đây */}
-          </Route>
-      </Routes>
-    </>
-  );
+    return (
+        <div className={styles.appContainer}>
+            <HeaderComponent/>
+            <ToastContainer autoClose={1000}/>
+            <main className={styles.mainContent}>
+                <Routes>
+                    <Route path={'/register'} element={<RegisterComponent/>}/>
+                    <Route path={'/login'} element={<LoginComponent/>}/>
+                    <Route path={'/'} element={<HomeComponent/>}/>
+                    <Route path="/" element={<PrivateRoute/>}>
+                        <Route path={'/user/detail/:id'} element={<DetailComponent/>}/>
+                        <Route path={'/account/change-password/:username'} element={<UpdateAccountComponent/>}/>
+                        {/* Thêm các route khác cần bảo vệ ở đây */}
+                    </Route>
+                </Routes>
+            </main>
+            <FooterComponent/>
+        </div>
+    );
 }
 
 export default App;
