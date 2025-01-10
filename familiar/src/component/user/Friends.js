c9ea744cf23d31278a3b398add4a9e4ff1fd7750
hungnguyen988 <hungnguuen988@gmail.com>
2025-01-10 08:13:23 +0700

    hung up

--- familiar/src/component/user/Friends.js
+++ familiar/src/component/user/Friends.js
@@ -25,6 +25,25 @@ export default function Friends({ friend, col, userId, setFriendList }) {
         }
     };
 
+
+    function formatUserName(fullName) {
+        const nameParts = fullName.trim().split(' ');
+
+        let formattedName = '';
+        let remainingLength = 14;
+
+        for (let i = nameParts.length - 1; i >= 0; i--) {
+            const part = nameParts[i];
+            if (formattedName.length + part.length + (formattedName ? 1 : 0) <= remainingLength) {
+                formattedName = part + (formattedName ? ' ' + formattedName : '');
+            } else {
+                break;
+            }
+        }
+
+        return formattedName;
+    }
+
     return (
         <div className={`col-12 col-sm-6 col-md-${col} mb-3`} key={friend.userId}>  {/* key ở đây */}
             <div className={`${styles.card} shadow-sm border-light hoverShadow`}>
@@ -36,7 +55,7 @@ export default function Friends({ friend, col, userId, setFriendList }) {
                     />
                     <Link to={`/users/detail/${friend.userId}`} style={{textDecoration: 'none'}}>
                         <p className="mx-2" style={{color: 'black'}}>
-                            {friend?.userFirstName} {friend?.userLastName}
+                            {formatUserName(friend?.userFirstName + " " + friend?.userLastName)}
                         </p>
                     </Link>
                     <div className="d-flex justify-content-center mt-3">
